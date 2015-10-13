3ff1d314ec98c7a38ee23cb8e49e451d8c9a8945
Karamaz0v1 <florent.guiotte@gmail.com>
2015-10-13 16:49:24 +0200

    init repo

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/***************************************************************************
+ *
+ * TODO: Project Title
+ *
+ * Author: Florent Guiotte <guiotte.florent@gmail.com>
+ *
+ **************************************************************************/
+
+#include <iostream>
+#include <visp/vpMatrix.h>
+
+int main( int argc, char* argv[] )
+{
+    return 0;
+}
+
+
